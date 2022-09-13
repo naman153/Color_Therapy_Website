076ab3f92dce9e8645fe54d92571807e75a12da4
Naman Gupta <153namangupta@gmail.com>
2022-09-13 18:34:25 +0530

    commit

--- medicine/settings.py
+++ medicine/settings.py
@@ -10,9 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
-import dj_database_url
-from django.conf.urls import url
-from django.urls import reverse_lazy
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
